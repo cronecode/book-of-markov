f84a1c885496a25217dc326df5d4cdb6fe52d5fe
Kristina Born <kristinab@nulogy.com>
2017-05-18 17:45:32 -0400

    implements read_file

--- lib/markov.rb
+++ lib/markov.rb
@@ -2,10 +2,14 @@ module Markov
   extend self
 
   def parse(text)
-    #text = File.read(file_name)
+
     text = text.gsub(/\n+/, ' ')
     text = text.gsub(/\./, '.@END')
     text = text.gsub(/\?/, '?@END')
     text = text.gsub(/!/, '!@END')
+  end
+
+  def read_file(file_name)
+    File.read(file_name)
   end
 end
