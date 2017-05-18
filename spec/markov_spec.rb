f23fa532dd7483246548cb8302fb3e04915502ee
Kristina Born <kristinab@nulogy.com>
2017-05-18 17:44:46 -0400

    red test for reading file

--- spec/markov_spec.rb
+++ spec/markov_spec.rb
@@ -20,13 +20,23 @@ RSpec.describe Markov do
       expect(text).to_not include("\n")
     end
 
-    fit "replaces sentence-terminating punctuation with markers" do
+    it "replaces sentence-terminating punctuation with markers" do
       sentence = "Who took all my cows? Asked Bob. I did!"
       desired_output = "Who took all my cows?@END Asked Bob.@END I did!@END"
 
       result = Markov.parse(sentence)
 
       expect(result).to eq(desired_output)
+    end
+  end
+
+  describe "#read_file" do
+    fit "reads a text file" do
+      file_name = "leviticus.txt"
+
+      text = Markov.read_file(file_name)
+
+      expect(text).to include("burnt-offering of the herd")
     end
   end
 end
